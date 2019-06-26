3625c2ebce19d88115ae08aaea2b7bb3ae6897d6
jackypy <jacqueline.parra7@gmail.com>
2019-06-25 23:56:14 -0400

    terminado y funcionando

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-def destroy
-  reset_session
-  redirect_to user_sing_up_path
+  def destroy
+    reset_session
+    redirect_to users_sign_up_path
   end
 
   def new
@@ -11,7 +11,7 @@ def destroy
   def create
     @user = User.find_by(email: params[:user][:email])
     if @user.nil?
-      redirect_to user_sing_in_path, alert: 'El usuario noexiste'
+      redirect_to users_sign_in_path, alert: 'El usuario no existe'
       return
     end
 
@@ -19,7 +19,7 @@ def destroy
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to user_sing_in_path, alert: 'El password no es valido'
+      redirect_to users_sign_in_path, alert: 'El password no es válido'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :histories
-  root 'histores#index'
+  root 'histories#index'
   get 'users/sign_up', to: "users#new"
   get 'users/sign_in', to: "sessions#new"
   post 'users', to: "users#create"
