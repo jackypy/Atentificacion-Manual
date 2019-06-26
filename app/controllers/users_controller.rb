77b0f44878c10bed6e9dfcf3c7501cc3e5883eca
jackypy <jacqueline.parra7@gmail.com>
2019-06-25 23:37:08 -0400

    users_controller correcion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
   def user_params
